b0aba2caa5817983448545894e6fa068e111fd2b
Paul Rieser <161761197+Asario2@users.noreply.github.com>
2026-03-11 12:50:42 +0100

    Update version date in starter_eleven.php

--- config/starter_eleven.php
+++ config/starter_eleven.php
@@ -5,6 +5,6 @@ return [
         'brandname' => 'Asarios Blog',
         'copyrightname' => '© '.date("Y").' MCSL von Asario & Oliver Reinking',
         'versionnr' => '4.89.6',
-        'versionsdatum' => '07.03.2026',
+        'versionsdatum' => '11.03.2026',
     ],
 ];
