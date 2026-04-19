15ac160150dd71702a4395dcd3b56f070a9764be
Asario2 <161761197+Asario2@users.noreply.github.com>
2026-04-19 13:17:06 +0200

    Hotfix - Newsletter to MSCL-Points not working (#456)

---
+++ resources/js/Application/Components/Social/Newsl_points_dup.vue
@@ -0,0 +1,34 @@
+<template>
+  <Layout>
+    <MetaHeader title="8 MCSL Points wurden deinem Konto gutgeschrieben" />
+    <div class="flex flex-col items-center justify-center h-screen space-y-4 mt-[-80px]">
+      <ErrorSVG color="#FFC900"></ErrorSVG>
+      <div class="text-center text-lg font-semibold">
+        Dieser Newsletter wurde schon verpunktet
+      </div>
+    </div>
+  </Layout>
+</template>
+
+<script>
+import MetaHeader from "@/Application/Homepage/Shared/MetaHeader.vue";
+import ErrorSVG from "@/Application/Components/Icons/ErrorSVG.vue";
+import Layout from "@/Application/Homepage/Shared/Layout.vue";
+import { toastBus } from "@/utils/toastBus";
+export default {
+components:{
+    ErrorSVG,
+    Layout,
+    MetaHeader,
+},
+props:{
+    i:[String,Number],
+},
+mounted()
+{
+    window.toastBus.emit( { status: 'points', message: 'Du hast 8 MCSL Points gesammelt'});
+}
+};
+
+</script>
+
