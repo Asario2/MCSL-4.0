d02564c9e94f4afadecd34caa5325aff63673634
Paul Rieser <161761197+Asario2@users.noreply.github.com>
2026-06-09 14:30:23 +0200

    Delete database/dumps/asd directory

--- database/dumps/asd/2/Newer_Data_ab_5.67.8.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `private_messages_text` ADD COLUMN `name` varchar(255) NULL;
-UPDATE `private_messages_text` SET `name` = '123asda' WHERE `id` = '1277';
-ALTER TABLE `xgen_activitylog` ADD COLUMN `dom` varchar(50) NULL;
-
