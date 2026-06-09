b342036b89bbbd9764e54dcd869460b8496bcf33
Asario2 <161761197+Asario2@users.noreply.github.com>
2026-06-09 14:32:50 +0200

    Refresh Git DB Dumbs (#507)

--- database/dumps/Newer_Data_ab_5.62.2.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE `privacy` ADD COLUMN `created_at` datetime NULL;
-ALTER TABLE `privacy` ADD COLUMN `updated_at` datetime NULL;
-ALTER TABLE `private_messages_text` ADD COLUMN `name` varchar(255) NULL;
-UPDATE `private_messages_text` SET `name` = '123asda' WHERE `id` = '1277';
-ALTER TABLE `users_rights` ADD COLUMN `xkis_FontographerToolz` tinyint(1) NOT NULL DEFAULT 0;
-ALTER TABLE `users_rights` ADD COLUMN `xkis_HackingLog` tinyint(1) NOT NULL DEFAULT 0;
-ALTER TABLE `xgen_activitylog` ADD COLUMN `dom` varchar(50) NULL;
--- database/dumps/asd/Newer_Data_ab_5.67.8.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE `privacy` ADD COLUMN `created_at` datetime NULL;
-ALTER TABLE `privacy` ADD COLUMN `updated_at` datetime NULL;
-ALTER TABLE `private_messages_text` ADD COLUMN `name` varchar(255) NULL;
-ALTER TABLE `users_rights` ADD COLUMN `xkis_FontographerToolz` tinyint(1) NOT NULL DEFAULT 0;
-ALTER TABLE `users_rights` ADD COLUMN `xkis_HackingLog` tinyint(1) NOT NULL DEFAULT 0;
-ALTER TABLE `xgen_activitylog` ADD COLUMN `dom` varchar(50) NULL;
-
